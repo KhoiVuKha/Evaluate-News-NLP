28dcd16683c675875856fc568464232ab67b06e5
Khoi Vu <khoivukha0509@gmail.com>
2022-11-29 22:24:32 +0700

    Done stage 2 - Setting up the MeaningCloud Sentiment Analysis API

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express()
 
@@ -20,3 +22,9 @@ app.listen(8080, function () {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+console.log(`Your API key is ${process.env.API_KEY}`);
+
+var textapi = new meaningCloudApi({
+    application_key: process.env.API_KEY
+});
